b7aaa94e0a5c1f1ee17e01be1bd998f53eb5ce0f
hemanth <hemanthjagaari@gmail.com>
2021-11-13 20:33:27 +0530

    redirect

--- shortURL/views.py
+++ shortURL/views.py
@@ -13,43 +13,17 @@ def generate_short(request):
     print("in generate method")
     if request.method=='POST':
         form=URL(request.POST)
-        print(form)
         print("its post")
         s_url=''.join(random.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890") for x in range(10))
-        #l_url=form.cleaned_dat['url']
-        l_url=""
         temp=request.POST.get("LONGURL","")
-        print("long url= ",temp)
+        print("long url= ",temp,"short_url =",s_url)
         l_url=temp
         new_url=URLmap(long_url=l_url,short_url=s_url)
         new_url.save()
-        short_url='http://localhost:8000/'+s_url
-        #form.instance.SHORTURL=s_url 
-        ##request.POST['SHORTURL']=s_url
+        short_url='http://127.0.0.1:8000/'+s_url
         context={'SHORTURL':short_url,'LONGURL':l_url}
         return render(request,'welcome.html',context)
-
-        """if form.is_valid():
-            s_url=''.join(random.choice(string.ascii_letters) for x in range(10))
-            #l_url=form.cleaned_dat['url']
-            l_url=""
-            temp=request.POS.get("LONGURL","")
-            print("long url= ",temp)
-            l_url=temp
-            new_url=URLmap(long_url=l_url,short_url=s_url)
-            new_url.save()
-            form.instance.SHORTURL=s_url 
-            request.shortURL.add(new_url)
-            return redirect(request,'welcome.html',{'SHORTURL':s_url})
-        else:
-            print(form.errors)
-            form=URL()
-            data=URLmap.objects.all()
-            context={
-                'form':form,'data':data
-            }
-            return render(request,'welcome.html',context)"""
 def urlRedirect(request,sh):
     print("short_url =",sh)
     data=URLmap.objects.get(short_url=sh)
-    return redirect('https://'+data.long_url)
+    return redirect(data.long_url)
